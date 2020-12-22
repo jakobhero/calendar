3fc91706d00dbf43aa386a9435ea63a9a8d9d813
Jakob Herold <jakob.herold.jh@gmail.com>
2020-12-22 18:17:28 +0100

    Multi-Calendar Functionality + Appnmts Endpoints

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from config import Config
-from models import User as DB_User,Appointment,db
-import secrets
+from models import User as DB_User,Appointment as DB_Appointment,Calendar as DB_Calendar, Association, db
+import secrets, datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,10 +20,10 @@ class Users(Resource):
         args=parser.parse_args()
         user_name=args["user"]
         if user_name == None:
-            return "Error! You need to provide argument 'user'",400
+            return "Error! No argument 'user' provided.",400
         
         if db.session.query(DB_User).filter(DB_User.name==user_name).scalar() is not None:
-            return "Error! User '"+user_name+"' already exists",404
+            return "Error! User '"+user_name+"' already exists.",404
 
         new_user=DB_User(name=user_name,secret=secrets.token_hex(nbytes=4))
         db.session.add(new_user)
@@ -40,11 +40,66 @@ class User(Resource):
             return "User '"+user_name+"' is not in the system",404
         return db.session.query(DB_User).filter(DB_User.name==user_name).first().to_dict(),200
 
+    def post(self,user_name):
+        if db.session.query(DB_User).filter(DB_User.name==user_name).scalar() is None:
+            return "User '"+user_name+"' is not in the system",404
+
+        args=parser.parse_args()
+        calendar_name=args["calendar"]
+        if calendar_name == None:
+            return "Error! No argument 'calendar' provided",400
+
+        if db.session.query(DB_Calendar).filter(DB_Calendar.owned_by==user_name,DB_Calendar.name==calendar_name).scalar() is not None:
+            return "Error! User '"+user_name+"' already has a calendar named '"+calendar_name+"'. Please choose a unique calendar name.",400
+        
+        new_calendar=DB_Calendar(name=calendar_name,owned_by=user_name)
+        db.session.add(new_calendar)
+        db.session.commit()
+        return {
+            'calendar':new_calendar.to_dict()
+        },200
+
+class Appointment(Resource):
+    def get(self,user_name,calendar_name):
+        if db.session.query(DB_User).filter(DB_User.name==user_name).scalar() is None:
+            return "User '"+user_name+"' is not in the system",404
+        
+        if db.session.query(DB_Calendar).filter(DB_Calendar.owned_by==user_name,DB_Calendar.name==calendar_name).scalar() is None:
+            return "Error! User '"+user_name+"' does not own a calendar named '"+calendar_name+"'.",400
+        
+        return db.session.query(DB_Calendar).filter(DB_Calendar.owned_by==user_name,DB_Calendar.name==calendar_name).first().to_dict()["appointments"],200
+    
+    def post(self,user_name,calendar_name):
+        if db.session.query(DB_User).filter(DB_User.name==user_name).scalar() is None:
+            return "User '"+user_name+"' is not in the system",404
+        
+        if db.session.query(DB_Calendar).filter(DB_Calendar.owned_by==user_name,DB_Calendar.name==calendar_name).scalar() is None:
+            return "Error! User '"+user_name+"' does not own a calendar named '"+calendar_name+"'.",400
+        
+        args=parser.parse_args()
+        name,start,duration=args["app_name"],datetime.datetime.fromtimestamp(args["app_start"]),args["app_dur"]
+
+        if name == None or start == None or duration == None:
+            return "Error! Make sure to provide the arguments 'app_name', 'app_start', and 'app_dur'"
+       
+        calendar_id=db.session.query(DB_Calendar).filter(DB_Calendar.owned_by==user_name,DB_Calendar.name==calendar_name).first().id
+        new_appointment=DB_Appointment(name=name,date=start,duration=duration,calendar_id=calendar_id)
+        db.session.add(new_appointment)
+        db.session.commit()
+        return new_appointment.to_dict()
+
+        
+
 api.add_resource(Users,'/users')
 api.add_resource(User,'/user/<user_name>')
+api.add_resource(Appointment,'/appointments/<user_name>/<calendar_name>')
 
 parser = reqparse.RequestParser()
 parser.add_argument('user',type=str)
+parser.add_argument('calendar',type=str)
+parser.add_argument('app_name',type=str)
+parser.add_argument('app_start',type=int)
+parser.add_argument('app_dur',type=int)
 
 if __name__=='__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -12,15 +12,21 @@ class User(db.Model):
     avail_start=db.Column(db.DateTime(),default=datetime.datetime(year=datetime.MINYEAR,month=1,day=1,hour=9,minute=0))
     avail_end=db.Column(db.DateTime(),default=datetime.datetime(year=datetime.MINYEAR,month=1,day=1,hour=18,minute=0))
     buffer=db.Column(db.SmallInteger,default=15)
-    appointments=db.relationship('Appointment',backref='owner', cascade="all, delete", lazy=True)
+    calendars_all=db.relationship('Association',backref='invited', cascade="all, delete", lazy=True)
+    calendars_own=db.relationship('Calendar',backref='owner',cascade="all, delete",lazy=True)
 
     def __repr__(self):
         return 'Name: '+self.name+', Secret: '+self.secret
 
     def to_dict(self):
-        appointments=[]
-        for appointment in self.appointments:
-            appointments.append(appointment.to_dict())
+        calendars_own,calendars_invited=[],[]
+        for calendar in self.calendars_own:
+            calendars_own.append(calendar.to_dict())
+        for association in self.calendars_all:
+            calendars_invited.append({
+                "calendar":association.calendar.to_dict(),
+                "status":association.status
+            })
         return {
             'name':self.name,
             'availability':{
@@ -28,15 +34,41 @@ class User(db.Model):
                 'start':self.avail_start.strftime("%H:%M"),
                 'end':self.avail_end.strftime("%H:%M")
             },
-            'appointments':appointments
+            'calendars_own':calendars_own,
+            'calendars_invited':calendars_invited
+        }
+
+class Association(db.Model):
+    __tablename__='association'
+    user_name=db.Column(db.String(20),db.ForeignKey("user.name"),primary_key=True)
+    calendar_id=db.Column(db.Integer,db.ForeignKey("calendar.id"),primary_key=True)
+    status=db.Column(db.String(20),default="Pending")
+
+class Calendar(db.Model):
+    __tablename__='calendar'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50),nullable=False)
+    owned_by=db.Column(db.String(8),db.ForeignKey('user.name'),nullable=False)
+    invited=db.relationship('Association',backref='calendar', cascade="all, delete", lazy=True)
+    appointments=db.relationship('Appointment',backref='calendar', cascade="all, delete", lazy=True)
+
+    def to_dict(self):
+        appointments=[]
+        for appointment in self.appointments:
+            appointments.append(appointment.to_dict())
+        return {
+            "name":self.name,
+            "owner":self.owned_by,
+            "appointments":appointments
         }
 
 class Appointment(db.Model):
     _tablename__='appointment'
-    name=db.Column(db.String(100),nullable=False,primary_key=True)
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100),nullable=False)
     date=db.Column(db.DateTime(),nullable=False)
     duration=db.Column(db.SmallInteger,nullable=False)
-    owner_id=db.Column(db.String(8),db.ForeignKey('user.secret'),nullable=False)
+    calendar_id=db.Column(db.String(8),db.ForeignKey('calendar.id'),nullable=False)
     
     def __repr__(self):
         return 'Name '+self.name+'\ntakes place on '+self.date+' for '+str(self.duration)+' mins.\nCreated by '+self.owner_id+'.'
